ec86333981c8a9d7e34a3457d70f483e3acf67e7
Chen Shao <chenshao@wlan-cl-kit-cs-172-17-3-28.scc.kit.edu>
2023-12-17 08:24:17 +0100

    transfer classs from repo

--- src/data_utils/hf_loader.py
+++ src/data_utils/hf_loader.py
@@ -8,13 +8,32 @@ from transformers import AutoModelForSequenceClassification, TrainingArguments, 
 from IPython import embed
 
 # adopted from https://github.com/ChenS676/TAPE/blob/main/core/LMs/lm_trainer.py
+import torch
+from src.data_utils.dataset import Dataset
+
 class Dataset(Dataset):
     """Dataset class for kg embedding for dataset:
 
     """
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
+    def __init__(self, X, cfg):
+        self.output_dir = f'output/{self.dataset_name}{self.use_gpt_str}/{self.model_name}-seed{self.seed}'
+        self.ckpt_dir = f'prt_lm/{self.dataset_name}{self.use_gpt_str}/{self.model_name}-seed{self.seed}'
+        data, num_classes, text = load_data(
+            dataset=self.dataset_name, use_text=True, use_gpt=cfg.lm.train.use_gpt, seed=self.seed)
+        self.data = data
+        self.num_nodes = data.y.size(0)
+        self.n_labels = num_classes
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        X = tokenizer(text, padding=True, truncation=True, max_length=512)
+        dataset = Dataset(X, data.y.tolist())
+        self.inf_dataset = dataset
+
+        self.train_dataset = torch.utils.data.Subset(
+            dataset, self.data.train_mask.nonzero().squeeze().tolist())
+        self.val_dataset = torch.utils.data.Subset(
+            dataset, self.data.val_mask.nonzero().squeeze().tolist())
+        self.test_dataset = torch.utils.data.Subset(
+            dataset, self.data.test_mask.nonzero().squeeze().tolist())
 
     def __len__(self):
         return len(self.y)
